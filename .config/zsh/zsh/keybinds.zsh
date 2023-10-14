52e0fd7d24dacd4f050f28498921fcc2efc3040f
James Coleman <grmrgecko@gmail.com>
2023-10-14 10:21:09 -0400

    Remove duplicats

--- .config/zsh/zsh/keybinds.zsh
+++
@@ -1,8 +0,0 @@
-WORDCHARS='~!#$%^&*(){}[]<>?.+;-'
-MOTION_WORDCHARS='~!#$%^&*(){}[]<>?.+;-/'
-
-''{back,for}ward-word() WORDCHARS=$MOTION_WORDCHARS zle .$WIDGET
-
-bindkey '^[[1;3C' forward-word
-bindkey '^[[1;3D' backward-word
-bindkey '^[^?' backward-kill-word
